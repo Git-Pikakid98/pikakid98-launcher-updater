0e755542563f9c39a2cba818498f42a21261aaa5
Pikakid98 <12392651+pikakid98@users.noreply.github.com>
2021-12-24 01:28:07 +0000

    Add files via upload

--- Updater.bat
+++ Updater.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 certutil.exe -urlcache -split -f "https://github.com/pikakid98/pikakid98-launcher-data/releases/download/Launcher/Launcher.bat" "Launcher.bat"
 
-START "" "cmd.exe" "Launcher.bat"
+START "" "Launcher.bat"
