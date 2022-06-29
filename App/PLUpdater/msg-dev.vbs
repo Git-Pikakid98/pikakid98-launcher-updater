4c36a72026f29f21bab2e0d96b52a48872576f1c
Pikakid98 <12392651+pikakid98@users.noreply.github.com>
2022-06-29 15:22:22 +0100

    Add files via upload

---
+++ App/PLUpdater/msg-dev.vbs
@@ -0,0 +1,1 @@
+answer = MsgBox("A new development version is available and will now be downloaded. Please keep in mind that this version may be quite buggy or unstable as it is currently being worked on to improve. Please report any bugs on the GitHub issues section at https://github.com/Git-Pikakid98/pikakid98-launcher/issues", vbDefaultButton1, "Pikakid98 Launcher [Dev Mode]")
--- App/PLUpdater/run.bat
+++ App/PLUpdater/run.bat
@@ -1,4 +1,25 @@
 @ECHO OFF
+if exist "dev.txt" (
+GOTO Dev
+) else (
+GOTO Stable
+)
+
+:Stable
 START /wait "" "..\App\PLUpdater\dl.vbs"
 timeout 2
 call "Launcher-22.bat"
+exit
+
+:Dev
+if exist "Launcher-Dev\Pikakid98.Launcher.exe" (
+call "Launcher-Dev.bat"
+exit
+) else (
+START /wait "" "..\App\PLUpdater\msg-dev.vbs"
+START /wait "" "..\App\PLUpdater\dl-dev.vbs"
+START /wait "" "..\App\PLUpdater\dl-dev2.vbs"
+timeout 2
+call "Launcher-Dev.bat"
+exit
+)
